c0ce2c61892fc64e838544fe4dede74e72911d6e
danishsadpara <danishsadapara@gmail.com>
2022-11-13 17:15:08 +0500

    redux and question text added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>React Redux</Text>
+      <Text style={styles.question}>What is Redux</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 103,
+    color: "white",
+    fontWeight: "bold",
+  },
+  question: {
+    color: "white",
+    letterSpacing: 10,
+    fontSize: 23,
   },
 });
